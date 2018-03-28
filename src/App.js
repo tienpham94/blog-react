4fa76763a9b391eb97654e7931704d7c0076e6ec
Tien Pham <r3dsky02@yahoo.com.vn>
2018-03-28 15:05:53 +0300

    Configure webpack babel

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import styles from './App.css'
+
+const App = () => (
+  <h1 className={styles.heading}>Hello World!</h1>
+)
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const { resolve } = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const DashboardPlugin = require('webpack-dashboard/plugin')
+
+const srcDir = resolve(__dirname, 'src')
+
+module.exports = {
+  entry: `${srcDir}/index.js`,
+  output: {
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+      enforce: 'pre',
+      test: /\.js$/,
+      loader: 'standard-loader',
+      exclude: /node_modules/
+    }, {
+      test: /\.js$/,
+      loader: 'babel-loader',
+      exclude: /node_modules/
+    }, {
+      test: /\.css$/,
+      loader: 'style-loader!css-loader?modules,localIdentName=[hash:base64:6]-[name]-[local]',
+      exclude: /node_modules/
+    }]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: `${srcDir}/index.html`
+    }),
+    new DashboardPlugin()
+  ]
+}
