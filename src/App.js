b52cb6e2b762036c406b3277447aba13f6ac5bcb
Tien Pham <r3dsky02@yahoo.com.vn>
2018-03-28 18:28:28 +0300

    Style the container

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react'
 
 import 'normalize.css'
-// import styles from './App.css'
+import styles from './App.css'
 import Header from './components/Header/Header'
 import Post from './components/Post/Post'
 
 const App = () => (
-  <Header />
+  <div>
+    <Header />
+    <div className={styles.container}>
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+    </div>
+  </div>
 )
 
 export default App
--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
+import styles from './Post.css'
+
 const Post = () => (
   <div>
-    <h2><a href='#'>Blog title</a></h2>
-    <p>Content</p>
+    <h2 className={styles.title}><a href='#'>Blog title</a></h2>
+    <p className={styles.content}>Content</p>
   </div>
 )
 
